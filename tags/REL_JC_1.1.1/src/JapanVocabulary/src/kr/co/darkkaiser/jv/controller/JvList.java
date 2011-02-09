71a6790a1a46778e5195f4488d29060fb98b9e50
DarkKaiser <DarkKaiser@eb2df1e2-efe3-6949-8084-3fc849c3e4eb>
2011-02-09 15:47:43 +0000

    git-svn-id: svn://192.168.219.100/torrent-auto-downloader@150 eb2df1e2-efe3-6949-8084-3fc849c3e4eb

---
+++ tags/REL_JC_1.1.1/src/JapanVocabulary/src/kr/co/darkkaiser/jv/controller/JvList.java
@@ -0,0 +1,9 @@
+package kr.co.darkkaiser.jv.controller;
+
+import kr.co.darkkaiser.jv.data.JapanVocabulary;
+
+public interface JvList {
+	public JapanVocabulary getCurrentVocabulary();
+	public JapanVocabulary previousVocabulary(StringBuilder sbErrMessage);
+	public JapanVocabulary nextVocabulary(StringBuilder sbErrMessage);
+}
