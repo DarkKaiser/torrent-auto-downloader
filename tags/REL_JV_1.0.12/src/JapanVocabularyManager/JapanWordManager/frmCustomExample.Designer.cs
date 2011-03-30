59b90380b304dd7224223d84b1696e3ae8f2c937
DarkKaiser <DarkKaiser@eb2df1e2-efe3-6949-8084-3fc849c3e4eb>
2011-03-30 16:48:09 +0000

    git-svn-id: svn://192.168.219.100/torrent-auto-downloader@193 eb2df1e2-efe3-6949-8084-3fc849c3e4eb

---
+++ tags/REL_JV_1.0.12/src/JapanVocabularyManager/JapanWordManager/frmCustomExample.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace JapanWordManager
+{
+    partial class frmCustomExample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtExample = new System.Windows.Forms.TextBox();
+            this.txtExampleTranslation = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.btnAddTag = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(407, 125);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "추가";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(488, 125);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "취소";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtExample
+            // 
+            this.txtExample.Location = new System.Drawing.Point(14, 24);
+            this.txtExample.Name = "txtExample";
+            this.txtExample.Size = new System.Drawing.Size(549, 21);
+            this.txtExample.TabIndex = 2;
+            this.txtExample.TextChanged += new System.EventHandler(this.txtExample_TextChanged);
+            // 
+            // txtExampleTranslation
+            // 
+            this.txtExampleTranslation.Location = new System.Drawing.Point(14, 98);
+            this.txtExampleTranslation.Name = "txtExampleTranslation";
+            this.txtExampleTranslation.Size = new System.Drawing.Size(549, 21);
+            this.txtExampleTranslation.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "예문";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "뜻";
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(14, 125);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(387, 88);
+            this.webBrowser.TabIndex = 6;
+            // 
+            // btnAddTag
+            // 
+            this.btnAddTag.Location = new System.Drawing.Point(284, 51);
+            this.btnAddTag.Name = "btnAddTag";
+            this.btnAddTag.Size = new System.Drawing.Size(279, 23);
+            this.btnAddTag.TabIndex = 7;
+            this.btnAddTag.Text = "현재 커서 위치에 <sup><font> 태그 추가";
+            this.btnAddTag.UseVisualStyleBackColor = true;
+            this.btnAddTag.Click += new System.EventHandler(this.btnAddTag_Click);
+            // 
+            // frmCustomExample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 223);
+            this.Controls.Add(this.btnAddTag);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtExampleTranslation);
+            this.Controls.Add(this.txtExample);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAdd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmCustomExample";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "frmCustomExample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtExample;
+        private System.Windows.Forms.TextBox txtExampleTranslation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button btnAddTag;
+    }
+}
