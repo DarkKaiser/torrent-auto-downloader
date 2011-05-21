25891f1c943d08fbd6a7f1fc0e0d08430adc43ae
DarkKaiser <DarkKaiser@eb2df1e2-efe3-6949-8084-3fc849c3e4eb>
2011-05-21 13:39:16 +0000

    # 일본어 단어 암기   - 1.0.15 배포함
    git-svn-id: svn://192.168.219.100/torrent-auto-downloader@214 eb2df1e2-efe3-6949-8084-3fc849c3e4eb

--- trunk/src/JapanVocabulary/src/kr/co/darkkaiser/jv/JvDefines.java
+++ trunk/src/JapanVocabulary/src/kr/co/darkkaiser/jv/JvDefines.java
@@ -90,6 +90,6 @@ public class JvDefines {
     /*
      * APP과 함께 배포되는 단어 DB 버전
      */
-    public static final String JV_DB_VERSION_FROM_ASSETS = "Jv.Voca.51";
+    public static final String JV_DB_VERSION_FROM_ASSETS = "Jv.Voca.52";
     
 }
