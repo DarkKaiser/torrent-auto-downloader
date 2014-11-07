7bd4d41cf3a934fd00b2208b99c5963a4c3e63cd
darkkaiser <darkkaiser@eb2df1e2-efe3-6949-8084-3fc849c3e4eb>
2014-11-07 02:23:22 +0000

    git-svn-id: svn://192.168.219.100/torrent-auto-downloader@313 eb2df1e2-efe3-6949-8084-3fc849c3e4eb

---
+++ trunk/solution_private/JapanVocabulary/src/JapanVocabulary/app/src/main/java/kr/co/darkkaiser/jv/helper/FileHash.java
@@ -0,0 +1,39 @@
+package kr.co.darkkaiser.jv.helper;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class FileHash {
+
+	public static byte[] getHash(File file) throws IOException, NoSuchAlgorithmException {
+		assert file != null;
+		assert file.exists() == true;
+
+	    BufferedInputStream bis = null;
+
+	    try {
+	        MessageDigest md = MessageDigest.getInstance("SHA1");
+	        bis = new BufferedInputStream(new FileInputStream(file));
+	        
+	        int readBytes;
+	        byte[] buffer = new byte[1024];
+	        while ((readBytes = bis.read(buffer)) != -1) {
+	            md.update(buffer, 0, readBytes);
+	        }
+
+	        return md.digest();
+	    } finally {
+	        if (bis != null) {
+	        	try {
+	        		bis.close();
+	        	} catch (IOException ignored) {
+	        	}
+	        }
+	    }
+	}
+
+}
