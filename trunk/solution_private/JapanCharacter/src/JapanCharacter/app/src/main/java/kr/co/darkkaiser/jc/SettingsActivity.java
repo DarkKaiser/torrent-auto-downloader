5aa2f0a4b04c4edf77f781eb7c796f3ae4957692
darkkaiser <darkkaiser@eb2df1e2-efe3-6949-8084-3fc849c3e4eb>
2015-08-11 04:41:51 +0000

    git-svn-id: svn://192.168.219.100/torrent-auto-downloader@666 eb2df1e2-efe3-6949-8084-3fc849c3e4eb

--- trunk/solution_private/JapanCharacter/src/JapanCharacter/app/src/main/java/kr/co/darkkaiser/jc/SettingsActivity.java
+++ trunk/solution_private/JapanCharacter/src/JapanCharacter/app/src/main/java/kr/co/darkkaiser/jc/SettingsActivity.java
@@ -1,20 +1,60 @@
 package kr.co.darkkaiser.jc;
 
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
-import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
-import android.preference.PreferenceActivity;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 
-public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
+import com.lb.material_preferences_library.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
+        setTheme(R.style.Theme_AppCompat_Jc_Settings);
         super.onCreate(savedInstanceState);
         getPreferenceManager().setSharedPreferencesName("jc_setup");
-        addPreferencesFromResource(R.xml.pref_settings_activity);
+
+
+
+        CheckBoxPreference chkHiragana = (CheckBoxPreference)findPreference("chk_hiragana");
+        CheckBoxPreference chkGatakana = (CheckBoxPreference)findPreference("chk_gatakana");
+        chkHiragana.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                boolean flag = (boolean) newValue;
+                CheckBoxPreference chkYoum = (CheckBoxPreference)findPreference("chk_youm");
+                CheckBoxPreference chkGatakana = (CheckBoxPreference)findPreference("chk_gatakana");
+
+                if (flag || chkGatakana.isChecked()) {
+                    chkYoum.setEnabled(true);
+                } else {
+                    chkYoum.setEnabled(false);
+                }
+
+                return true;
+            }
+        });
+        chkGatakana.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                boolean flag = (boolean) newValue;
+
+                CheckBoxPreference chkYoum = (CheckBoxPreference)findPreference("chk_youm");
+                CheckBoxPreference chkHiragana = (CheckBoxPreference)findPreference("chk_hiragana");
+
+                if (chkHiragana.isChecked() || flag) {
+                    chkYoum.setEnabled(true);
+                } else {
+                    chkYoum.setEnabled(false);
+                }
+
+                return true;
+            }
+        });
 
         CheckBoxPreference chkYoum = (CheckBoxPreference)findPreference("chk_youm");
         if (getPreferenceScreen().getSharedPreferences().getBoolean("chk_hiragana", true) || getPreferenceScreen().getSharedPreferences().getBoolean("chk_gatakana", true)) {
@@ -27,7 +67,7 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
         String versionName = getString(R.string.unknown_program_version);
         try {
             versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
-        } catch (NameNotFoundException e) {
+        } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
 
@@ -36,33 +76,8 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        CheckBoxPreference chkYoum = (CheckBoxPreference)findPreference("chk_youm");
-        CheckBoxPreference chkHiragana = (CheckBoxPreference)findPreference("chk_hiragana");
-        CheckBoxPreference chkGatakana = (CheckBoxPreference)findPreference("chk_gatakana");
-
-        if (chkHiragana.isChecked() || chkGatakana.isChecked()) {
-            chkYoum.setEnabled(true);
-        } else {
-            chkYoum.setEnabled(false);
-        }
+    protected int getPreferencesXmlId() {
+        return R.xml.pref_settings_activity;
     }
 
 }
