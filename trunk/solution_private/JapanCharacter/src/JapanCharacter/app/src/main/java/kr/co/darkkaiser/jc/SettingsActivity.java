eed206957aef6794c7bafe7dff2daac064a164c3
darkkaiser <darkkaiser@eb2df1e2-efe3-6949-8084-3fc849c3e4eb>
2015-08-28 01:07:06 +0000

    git-svn-id: svn://192.168.219.100/torrent-auto-downloader@705 eb2df1e2-efe3-6949-8084-3fc849c3e4eb

--- trunk/solution_private/JapanCharacter/src/JapanCharacter/app/src/main/java/kr/co/darkkaiser/jc/SettingsActivity.java
+++ trunk/solution_private/JapanCharacter/src/JapanCharacter/app/src/main/java/kr/co/darkkaiser/jc/SettingsActivity.java
@@ -2,6 +2,7 @@ package kr.co.darkkaiser.jc;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,6 +11,16 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         getSupportFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
