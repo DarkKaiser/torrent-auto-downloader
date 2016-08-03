edda72c9370a955f1a987fb88d92ebf77c6c1a8d
darkkaiser <darkkaiser@eb2df1e2-efe3-6949-8084-3fc849c3e4eb>
2016-08-03 08:22:56 +0000

    git-svn-id: svn://192.168.219.100/torrent-auto-downloader@728 eb2df1e2-efe3-6949-8084-3fc849c3e4eb

--- trunk/solution_private/TorrentAD/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/session/Login.java
+++ trunk/solution_private/TorrentAD/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/session/Login.java
@@ -1,5 +1,15 @@
 package kr.co.darkkaiser.torrentad.session;
 
+import org.jsoup.Connection;
+
 public class Login implements LoginI {
+
+	private Connection.Response aaa = null;
+
+	public void login() {
+	}
+
+	public void logout() {
+	}
 
 }
--- trunk/solution_private/TorrentAD/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/session/LoginI.java
+++ trunk/solution_private/TorrentAD/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/session/LoginI.java
@@ -2,4 +2,8 @@ package kr.co.darkkaiser.torrentad.session;
 
 public interface LoginI {
 
+	public void login();
+	
+	public void logout();
+	
 }
