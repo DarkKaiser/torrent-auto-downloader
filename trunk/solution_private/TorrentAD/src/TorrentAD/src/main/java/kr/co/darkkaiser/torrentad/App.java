c58f8aa859936da38356438bd394cddd4deae0a5
darkkaiser <darkkaiser@eb2df1e2-efe3-6949-8084-3fc849c3e4eb>
2016-08-03 08:08:44 +0000

    git-svn-id: svn://192.168.219.100/torrent-auto-downloader@724 eb2df1e2-efe3-6949-8084-3fc849c3e4eb

--- trunk/solution_private/TorrentAD/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/App.java
+++ trunk/solution_private/TorrentAD/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/App.java
@@ -1,7 +1,18 @@
 package kr.co.darkkaiser.torrentad;
 
+import com.google.gson.Gson;
+
 public class App {
 	public static void main(String[] args) {
+		Gson gson = new Gson();
+//		String json = "{'name':'김태희', 'phone_number':'010-1234-5678'}";
+//		Person java = gson.fromJson(json, Person.class);
+		  
 		System.out.println("Hello World!");
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				System.out.println("shutdown");
+			}
+		});
 	}
 }
---
+++ trunk/solution_private/TorrentAD/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/session/Login.java
@@ -0,0 +1,5 @@
+package kr.co.darkkaiser.torrentad.session;
+
+public class Login {
+
+}
