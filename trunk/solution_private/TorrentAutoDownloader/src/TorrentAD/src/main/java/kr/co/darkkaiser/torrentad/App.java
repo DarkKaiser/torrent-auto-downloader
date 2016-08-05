43475d46b6a0fa7513258020ddfc9d3a420b28b7
darkkaiser <darkkaiser@eb2df1e2-efe3-6949-8084-3fc849c3e4eb>
2016-08-05 02:34:36 +0000

    git-svn-id: svn://192.168.219.100/torrent-auto-downloader@741 eb2df1e2-efe3-6949-8084-3fc849c3e4eb

--- trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/App.java
+++ trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/App.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 
 import com.google.gson.Gson;
 
+import kr.co.darkkaiser.torrentad.common.Constants;
 import kr.co.darkkaiser.torrentad.config.Setting;
 import kr.co.darkkaiser.torrentad.website.BogoBogoWebSite;
 import kr.co.darkkaiser.torrentad.website.BogoBogoWebSiteAccount;
@@ -14,63 +15,9 @@ import kr.co.darkkaiser.torrentad.website.BogoBogoWebSiteAccount;
 public class App {
 	
 	private static final Logger logger = LoggerFactory.getLogger(App.class);
-	
-    private void addShutdownHook(final App app) {
-//        Runnable shutdownHook = new Runnable() {
-//            public void run() {
-//            	logger.info("FBISDispatcher 서버를 종료하는 중입니다...");
-//                app.stop();
-//            }
-//        };
-//
-//        // add shutdown hook
-//        Runtime runtime = Runtime.getRuntime();
-//        runtime.addShutdownHook(new Thread(shutdownHook));
-    }
-    
-	public static void main(String[] args) {
-		logger.info("FBISDispatcher 서버가 종료되었습니다.");
-		
-		App main = new App();
-//		// 환경설정 파일명을 읽어들인다.
-//		String configFilePath = Constants.DISPATCHER_SERVER_CONFIG_FILEPATH;
-//		if (args.length > 0) {
-//			configFilePath = args[0];
-//		}
-//
-//		try {
-//			FbisDispatcherServer server = new FbisDispatcherServer();
-//			if (server != null) {
-//				StringBuilder sb = new StringBuilder();
-//				sb.append("\n")
-//				  .append("##################################################\n")
-//				  .append("###                                            ###\n")
-//				  .append("###       FBISDispatcher 서버 Version 0.1       ###\n")
-//				  .append("###                                            ###\n")
-//				  .append("###              developed by DarkKaiser       ###\n")
-//				  .append("###                                            ###\n")
-//				  .append("##################################################\n");
-//				logger.info(sb.toString());
-//
-//				// start the server
-//				if (server.start(configFilePath) == true) {
-//					logger.info("FBISDispatcher 서버가 시작되었습니다.");
-//
-//		            // add shutdown hook if possible
-//		            main.addShutdownHook(server);
-//				} else {
-//					server.stop();
-//
-//					logger.info("FBISDispatcher 서버가 종료되었습니다.");
-//				}
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			
-//			logger.info("FBISDispatcher 서버가 종료되었습니다.", e);
-//		}
-		/////////////////////////////////////////////////////////////////////
-		
+
+	private boolean start(String configFilePath) {
+		// @@@@@
 		Gson gson = new Gson();
 		String json = "{'domain':'zipbogo.net', 'phone_number':'010-1234-5678'}";
 
@@ -93,12 +40,56 @@ public class App {
 		
 		l.logout();
 		
-		System.out.println("Hello World!");
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			public void run() {
-				System.out.println("shutdown");
-			}
-		});
+		return true;
 	}
 	
+	private void stop() {
+		// @@@@@
+	}
+
+    private void addShutdownHook(final App app) {
+        Runnable shutdownHook = new Runnable() {
+            public void run() {
+            	logger.info("{} 프로그램을 종료하는 중입니다...", Constants.APP_NAME);
+                app.stop();
+            }
+        };
+
+        // add shutdown hook
+        Runtime runtime = Runtime.getRuntime();
+        runtime.addShutdownHook(new Thread(shutdownHook));
+    }
+    
+	public static void main(String[] args) {
+		try {
+			App app = new App();
+			if (app != null) {
+				StringBuilder sb = new StringBuilder();
+				sb.append("\n")
+				  .append("########################################################\n")
+				  .append("###                                                  ###\n")
+				  .append("###             ").append(Constants.APP_NAME).append(" Version ").append(Constants.APP_VERSION).append("              ###\n")
+				  .append("###                                                  ###\n")
+				  .append("###             Developed by DarkKaiser              ###\n")
+				  .append("###                                                  ###\n")
+				  .append("########################################################\n");
+				logger.info(sb.toString());
+
+				// start the server
+				if (app.start(Constants.DISPATCHER_SERVER_CONFIG_FILEPATH) == true) {
+					logger.info("{} 프로그램이 시작되었습니다.", Constants.APP_NAME);
+
+		            // add shutdown hook if possible
+		            app.addShutdownHook(app);
+				} else {
+					app.stop();
+
+					logger.error("{} 프로그램이 종료되었습니다.", Constants.APP_NAME);
+				}
+			}
+		} catch (Exception e) {
+			logger.error("{}가 종료되었습니다.", Constants.APP_NAME, e);
+		}
+	}
+
 }
