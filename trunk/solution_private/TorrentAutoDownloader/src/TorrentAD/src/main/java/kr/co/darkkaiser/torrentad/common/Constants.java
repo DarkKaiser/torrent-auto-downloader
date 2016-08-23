fd005ddd5955838e20348d47132469cb415aedd1
darkkaiser <darkkaiser@eb2df1e2-efe3-6949-8084-3fc849c3e4eb>
2016-08-23 05:02:27 +0000

    git-svn-id: svn://192.168.219.100/torrent-auto-downloader@771 eb2df1e2-efe3-6949-8084-3fc849c3e4eb

--- trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/common/Constants.java
+++ trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/common/Constants.java
@@ -9,7 +9,10 @@ public final class Constants {
 	public static final String APP_CONFIG_FILE_NAME = "torrentad.xml";
 
 	public static final String APP_CONFIG_KEY_TASK_EXECUTE_INTERVAL_TIME_SECOND = "task-execute-interval-time-second";
-	
+		
+	/** 비밀번호를 암복호화 하기 위한 키(최소 16자 이상) */
+	public static final String CRYPTOGRAPH_KEY = "chlrhrkehlwk9876@*!";
+
 	private Constants() {
 	}
 
