aff6cf8785ba4715fed9a1c7049f484e8631dea8
darkkaiser <darkkaiser@eb2df1e2-efe3-6949-8084-3fc849c3e4eb>
2016-09-21 08:45:23 +0000

    git-svn-id: svn://192.168.219.100/torrent-auto-downloader@915 eb2df1e2-efe3-6949-8084-3fc849c3e4eb

--- trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/net/torrent/transmission/TorrentAddMethodResult.java
+++ trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/net/torrent/transmission/TorrentAddMethodResult.java
@@ -4,22 +4,63 @@ import com.google.gson.annotations.SerializedName;
 
 import kr.co.darkkaiser.torrentad.net.torrent.transmission.methodresult.AbstractMethodResult;
 
+// @@@@@
 public final class TorrentAddMethodResult extends AbstractMethodResult {
-	private final class TorreadAddMethodResultArgument2 {
-		private String hashString;
-		private long id;
-		private String name;
-	}
-	
-	private final class TorreadAddMethodResultArgument {
-		@SerializedName("torrent-added")
-		private TorreadAddMethodResultArgument2 torrentadded = new TorreadAddMethodResultArgument2();
-	}
-	
-	private TorreadAddMethodResultArgument arguments;
 
-	// @@@@@
-//	{"arguments":{"torrent-added":{"hashString":"01c95c6e645279cac028e2316584b425249604d3","id":5,"name":"\ud0a5\ubcf5\uc11c (Kickboxer, 2016) 1080p WEB-DL"}},
-//	"result":"success"}
+	private final class Argument {
+		
+		@SerializedName("torrent-added")
+		private TorreatAdded torrentadded;
+
+		public long getId() {
+			return this.torrentadded.getId();
+		}
+		
+		public String getName() {
+			return this.torrentadded.getName();
+		}
+
+		public String getHashString() {
+			return this.torrentadded.getHashString();
+		}
+
+	}
+	
+	private final class TorreatAdded {
+				
+		private long id;
+		
+		private String name;
+		
+		private String hashString;
+
+		public long getId() {
+			return this.id;
+		}
+		
+		public String getName() {
+			return this.name;
+		}
+		
+		public String getHashString() {
+			return this.hashString;
+		}		
+	
+	}
+
+	// @@@@@ 배열 아닌가?
+	private Argument arguments;
+	
+	public long getId() {
+		return this.arguments.getId();
+	}
+	
+	public String getName() {
+		return this.arguments.getName();
+	}
+	
+	public String getHashString() {
+		return this.arguments.getHashString();
+	}
 
 }
--- trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/service/supervisorycontrol/action/TorrentSupervisoryControlActionImpl.java
+++ trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/service/supervisorycontrol/action/TorrentSupervisoryControlActionImpl.java
@@ -11,16 +11,57 @@ public class TorrentSupervisoryControlActionImpl extends AbstractAction implemen
 	@Override
 	protected void beforeExecute() {
 		// @@@@@
+//		this.transmitters.add(new TorrentFileTransmitter(this.configuration));
+//		this.transmitters.add(new FTPFileTransmitter(this.configuration));
 	}
 
 	@Override
 	protected void afterExecute() {
 		// @@@@@
+//		Iterator<FileTransmitter> iterator = this.transmitters.iterator();
+//		while (iterator.hasNext()) {
+//			iterator.next().transmitFinished();
+//		}
+//
+//		this.transmitters.clear();
+//
+//		// 전송이 성공한 파일들은 삭제한다.
+//		for (Map.Entry<File, Boolean> entry : this.files.entrySet()) {
+//			if (entry.getValue() == true) {
+//				logger.debug("{} 파일의 전송이 완료되어 삭제합니다.", entry.getKey().getName());
+//				entry.getKey().delete();
+//			}
+//		}
 	}
 
 	@Override
 	protected void execute() throws Exception {
 		// @@@@@
+//		for (Map.Entry<File, Boolean> entry : this.files.entrySet()) {
+//			File file = entry.getKey();
+//
+//			assert entry.getValue() == false;
+//
+//			try {
+//				Iterator<FileTransmitter> iterator = this.transmitters.iterator();
+//				while (iterator.hasNext()) {
+//					FileTransmitter transmitter = iterator.next();
+//					if (transmitter.support(file) == true) {
+//						transmitter.prepare();
+//						if (transmitter.transmit(file) == true) {
+//							entry.setValue(true);
+//							logger.debug("{} 파일의 전송이 완료되었습니다.", file.getName());
+//						} else {
+//							logger.warn("{} 파일의 전송이 실패하였습니다.", file.getName());
+//						}
+//
+//						break;
+//					}
+//				}
+//			} catch (Exception e) {
+//				logger.error("파일을 전송하는 도중에 예외가 발생하였습니다.({})", file.getName(), e);
+//			}
+//		}
 	}
 
 	@Override
