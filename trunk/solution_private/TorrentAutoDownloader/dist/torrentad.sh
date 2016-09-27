801be2938924f4f3f5994c90593411ca0fddf9eb
darkkaiser <darkkaiser@eb2df1e2-efe3-6949-8084-3fc849c3e4eb>
2016-09-27 02:00:59 +0000

    git-svn-id: svn://192.168.219.100/torrent-auto-downloader@956 eb2df1e2-efe3-6949-8084-3fc849c3e4eb

--- trunk/solution_private/TorrentAutoDownloader/dist/torrentad.sh
+++ trunk/solution_private/TorrentAutoDownloader/dist/torrentad.sh
@@ -12,14 +12,34 @@ stop)
     echo "$SRV_NAME 서버가 다수 실행중입니다. 확인하여 주세요."
     exit 1
   elif [ $PROCESS_COUNT -eq 1 ]; then
-    echo "$SRV_NAME 서버를 중지합니다..."
+    echo "$SRV_NAME 서버를 중지합니다. 잠시만 기다려주세요..."
 
     kill `ps ax | grep -v grep | grep java | grep $SRV_NAME | awk '{print $1}'`
 
-    sleep 1
-	
+    index=1
+    while [ $index -le 3 ]
+    do
+      sleep 1
+
+      PROCESS_COUNT=`ps ax | grep -v grep | grep java | grep $SRV_NAME | wc -l`
+      if [ $PROCESS_COUNT -eq 0 ]; then
+        break
+      fi
+
+      index=$((index + 1))
+    done
+
     PROCESS_COUNT=`ps ax | grep -v grep | grep java | grep $SRV_NAME | wc -l`
-    [ $PROCESS_COUNT -ne 0 ] && echo "$SRV_NAME 서버의 중지가 실패하였습니다..." || echo "$SRV_NAME 서버가 중지되었습니다."
+    if [ $PROCESS_COUNT -ne 0 ]; then
+      kill -9 `ps ax | grep -v grep | grep java | grep $SRV_NAME | awk '{print $1}'`
+
+      sleep 1
+
+      PROCESS_COUNT=`ps ax | grep -v grep | grep java | grep $SRV_NAME | wc -l`
+      [ $PROCESS_COUNT -ne 0 ] && echo "$SRV_NAME 서버의 중지가 실패하였습니다..." || echo "$SRV_NAME 서버가 중지되었습니다."
+    else
+      echo "$SRV_NAME 서버가 중지되었습니다."
+    fi
   else
     echo "$SRV_NAME 서버가 실행중인 상태가 아닙니다."
     exit 1
@@ -28,8 +48,7 @@ stop)
 
 *)
   PROCESS_COUNT=`ps ax | grep -v grep | grep java | grep $SRV_NAME | wc -l`
-  if [ $PROCESS_COUNT -ne 0 ]
-  then
+  if [ $PROCESS_COUNT -ne 0 ]; then
     echo "$SRV_NAME 서버가 이미 실행중입니다."
   else
     echo "$SRV_NAME 서버가 실행되었습니다."
