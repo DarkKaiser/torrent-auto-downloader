5c63cc009b8b12912ef9104089936e868ae29ba3
darkkaiser <darkkaiser@eb2df1e2-efe3-6949-8084-3fc849c3e4eb>
2016-10-05 01:10:28 +0000

    git-svn-id: svn://192.168.219.100/torrent-auto-downloader@979 eb2df1e2-efe3-6949-8084-3fc849c3e4eb

--- trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/website/WebSiteBoard.java
+++ trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/website/WebSiteBoard.java
@@ -3,13 +3,13 @@ package kr.co.darkkaiser.torrentad.website;
 public interface WebSiteBoard {
 
 	String getName();
-	
+
 	String getDescription();
-	
+
 	String getURL();
 
-	String getDefaultRegistDateFormatString();
-	
 	int getDefaultLoadPageCount();
+
+	String getDefaultRegistDateFormatString();
 
 }
--- trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/website/impl/bogobogo/BogoBogoBoard.java
+++ trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/website/impl/bogobogo/BogoBogoBoard.java
@@ -44,19 +44,19 @@ public enum BogoBogoBoard implements WebSiteBoard {
 	}
 
 	/**
-	 * 게시판 목록의 등록일자 포맷을 반환합니다.
-	 */
-	@Override
-	public String getDefaultRegistDateFormatString() {
-		return "yy-MM-dd";
-	}
-
-	/**
 	 * 게시판에서 읽어들일 페이지 수, 읽어들인 페이수에 해당하는 게시물을 이용하여 검색 작업을 수행한다.
 	 */
 	@Override
 	public int getDefaultLoadPageCount() {
 		return 5;
+	}
+
+	/**
+	 * 게시판 목록의 등록일자 포맷을 반환합니다.
+	 */
+	@Override
+	public String getDefaultRegistDateFormatString() {
+		return "yy-MM-dd";
 	}
 
 	public boolean hasCategory() {
