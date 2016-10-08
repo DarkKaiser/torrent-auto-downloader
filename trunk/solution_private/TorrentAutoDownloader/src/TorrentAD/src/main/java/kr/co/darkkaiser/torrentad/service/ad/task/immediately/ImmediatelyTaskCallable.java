b108d9916724227b89adaa09f8114a4956472993
darkkaiser <darkkaiser@eb2df1e2-efe3-6949-8084-3fc849c3e4eb>
2016-10-08 19:37:14 +0000

    git-svn-id: svn://192.168.219.100/torrent-auto-downloader@1001 eb2df1e2-efe3-6949-8084-3fc849c3e4eb

--- trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/service/ad/task/immediately/ImmediatelyTaskCallable.java
+++ trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/service/ad/task/immediately/ImmediatelyTaskCallable.java
@@ -1,8 +1,14 @@
 package kr.co.darkkaiser.torrentad.service.ad.task.immediately;
 
-public interface ImmediatelyTaskCallable {
+import java.util.concurrent.Callable;
 
-	// @@@@@
+// @@@@@ 인터페이스명
+public interface ImmediatelyTaskCallable extends Callable<Boolean> {
+
 	String getDescription();
-	
+
+	void validate();
+
+	boolean isValid();
+
 }
--- trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/service/ad/task/immediately/ImmediatelyTaskImpl.java
+++ trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/service/ad/task/immediately/ImmediatelyTaskImpl.java
@@ -1,5 +1,8 @@
 package kr.co.darkkaiser.torrentad.service.ad.task.immediately;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import kr.co.darkkaiser.torrentad.service.ad.task.AbstractTask;
 import kr.co.darkkaiser.torrentad.service.ad.task.TaskMetadataRegistry;
 import kr.co.darkkaiser.torrentad.service.ad.task.TaskResult;
@@ -7,7 +10,9 @@ import kr.co.darkkaiser.torrentad.service.ad.task.TaskType;
 
 public class ImmediatelyTaskImpl extends AbstractTask implements ImmediatelyTask {
 
-	// @@@@@
+	private static final Logger logger = LoggerFactory.getLogger(ImmediatelyTaskImpl.class);
+	
+	// @@@@@ 변수명
 	protected ImmediatelyTaskCallable callable;
 	
 	public ImmediatelyTaskImpl(String taskId, String taskDescription, TaskMetadataRegistry taskMetadataRegistry) {
@@ -16,19 +21,28 @@ public class ImmediatelyTaskImpl extends AbstractTask implements ImmediatelyTask
 
 	@Override
 	public TaskResult run() {
-		// @@@@@
-//		callable.run();
-		
-		return null;
+		validate();
+
+		try {
+			if (this.callable.call() == false)
+				return TaskResult.FAILED;
+		} catch (Exception e) {
+			logger.error(null, e);
+			return TaskResult.UNEXPECTED_EXCEPTION;
+		}
+
+		return TaskResult.OK;
 	}
 
 	@Override
 	public void validate() {
 		super.validate();
 
-		// @@@@@
+		// @@@@@ 변수명
 		if (this.callable == null)
 			throw new NullPointerException("callable");
+
+		this.callable.validate();
 	}
 
 	@Override
@@ -36,6 +50,7 @@ public class ImmediatelyTaskImpl extends AbstractTask implements ImmediatelyTask
 		return new StringBuilder()
 				.append(ImmediatelyTaskImpl.class.getSimpleName())
 				.append("{")
+				.append("callable:").append(this.callable)//@@@@@ 변수명
 				.append("}, ")
 				.append(super.toString())
 				.toString();
