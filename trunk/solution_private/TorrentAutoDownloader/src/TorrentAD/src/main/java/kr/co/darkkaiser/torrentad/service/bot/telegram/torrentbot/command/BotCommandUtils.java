b53d7caf246fc78bcb006622ddca8c4bd8960483
darkkaiser <darkkaiser@eb2df1e2-efe3-6949-8084-3fc849c3e4eb>
2016-11-01 16:06:18 +0000

    git-svn-id: svn://192.168.219.100/torrent-auto-downloader@1068 eb2df1e2-efe3-6949-8084-3fc849c3e4eb

--- trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/service/bot/telegram/torrentbot/command/BotCommandUtils.java
+++ trunk/solution_private/TorrentAutoDownloader/src/TorrentAD/src/main/java/kr/co/darkkaiser/torrentad/service/bot/telegram/torrentbot/command/BotCommandUtils.java
@@ -5,12 +5,21 @@ import java.util.Arrays;
 import org.jsoup.helper.StringUtil;
 
 import kr.co.darkkaiser.torrentad.util.OutParam;
+import kr.co.darkkaiser.torrentad.website.WebSiteBoardItem;
 
 public final class BotCommandUtils {
 
+	// TelegramBot 커맨드의 최대 길이
 	public static final int COMMAND_MAX_LENGTH = 32;
+
+	// TelegramBot 커맨드 이니셜 문자
 	public static final String COMMAND_INITIAL_CHARACTER = "/";
+
+	// TelegramBot 커맨드 또는 파라메터간의 구분자
     public static final String COMMAND_PARAMETER_SEPARATOR = " ";
+
+    // 하나의 커맨드에 파라메터가 포함되어 있는 형식을 가지는 OneComplexCommand의 구분자
+    public static final String ONE_COMPLEX_COMMAND_SEPARATOR = "_";
 
     public static final void parse(String message, final OutParam<String> outCommand, final OutParam<String[]> outParameters, final OutParam<Boolean> outContainInitialChar) {
 		if (StringUtil.isBlank(message) == true)
@@ -31,8 +40,24 @@ public final class BotCommandUtils {
 			outContainInitialChar.set(false);
 		}
 
+		// OneComplexCommand인지 확인한다.
+		if (messageArrays.length == 1) {
+			String[] commandArrays = command.split(ONE_COMPLEX_COMMAND_SEPARATOR);
+			if (commandArrays.length > 1) {
+				outCommand.set(commandArrays[0]);
+				outParameters.set(Arrays.copyOfRange(commandArrays, 1, commandArrays.length));
+				return;
+			}
+		}
+
 		outCommand.set(command);
 		outParameters.set(Arrays.copyOfRange(messageArrays, 1, messageArrays.length));
+    }
+    
+    // @@@@@ 이 함수를 여기다 둬야하나???
+    // 가변인자를 둬서 들어온 순서대로 만들어주는건??? 이 함수를 이용하는 쪽에서 순서나 그런건 정함
+    public static final String test(WebSiteBoardItem boardItem) {
+    	return String.format("%s%s%s", boardItem.getBoard().getCode(), ONE_COMPLEX_COMMAND_SEPARATOR, boardItem.getIdentifier());
     }
 
 	private BotCommandUtils() {
