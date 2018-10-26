895ccba11f4f8136a5f60f2863a540f8e8bce432
darkkaiser <darkkaiser@eb2df1e2-efe3-6949-8084-3fc849c3e4eb>
2018-10-26 01:54:16 +0000

    git-svn-id: svn://192.168.219.100/torrent-auto-downloader@1306 eb2df1e2-efe3-6949-8084-3fc849c3e4eb

--- trunk/src/TorrentAD/src/main/java/com/darkkaiser/torrentad/website/impl/totoria/Totoria.java
+++ trunk/src/TorrentAD/src/main/java/com/darkkaiser/torrentad/website/impl/totoria/Totoria.java
@@ -1,9 +1,8 @@
 package com.darkkaiser.torrentad.website.impl.totoria;
 
+import com.darkkaiser.torrentad.common.Constants;
 import com.darkkaiser.torrentad.util.Tuple;
 import com.darkkaiser.torrentad.website.*;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.apache.http.HttpStatus;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -14,6 +13,8 @@ import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -28,11 +29,6 @@ public class Totoria extends AbstractWebSite {
 
 	private static final String LOGIN_PROCESS_URL_1 = String.format("%s/login_check.php", BASE_URL_WITH_DEFAULT_PATH);
 
-    // @@@@@
-	private static final String DOWNLOAD_PROCESS_URL_1 = String.format("%s/download.php", BASE_URL_WITH_DEFAULT_PATH);
-	private static final String DOWNLOAD_PROCESS_URL_2 = "http://linktender.net/";
-	private static final String DOWNLOAD_PROCESS_URL_3 = "http://linktender.net/execDownload.php";
-
 	private Connection.Response loginConnResponse;
 
 	// 조회된 결과 목록
@@ -40,27 +36,6 @@ public class Totoria extends AbstractWebSite {
 
 	// 검색된 결과 목록
 	private List<TotoriaSearchResultData> searchResultDataList = new LinkedList<>();
-
-	// @@@@@
-	private final class DownloadProcess1Result {
-
-		private String stat;
-		private String key;
-		private String msg;
-
-		public String getStat() {
-			return this.stat;
-		}
-
-		public String getKey() {
-			return this.key;
-		}
-
-		public String getMsg() {
-			return this.msg;
-		}
-
-	}
 
 	public Totoria(final String owner, final String downloadFileWriteLocation) {
 		this(null, owner, downloadFileWriteLocation);
@@ -490,7 +465,6 @@ public class Totoria extends AbstractWebSite {
                 Elements afElements = elements.get(3).getElementsByTag("a");
                 if (afElements.isEmpty() == true)
                     throw new ParseException(String.format("게시물에서 추출된 첨부파일에 대한 정보가 0건입니다. CSS셀렉터를 확인하세요.(URL:%s)", detailPageURL), 0);
-
 				try {
                     String[] exceptFileExtension = { "JPG", "JPEG", "GIF", "PNG" };
 
@@ -529,7 +503,6 @@ public class Totoria extends AbstractWebSite {
 
 		int downloadTryCount = 0;
 		int downloadCompletedCount = 0;
-		Gson gson = new GsonBuilder().create();
 		String detailPageURL = boardItem.getDetailPageURL();
 
 		assert StringUtil.isBlank(detailPageURL) == false;
@@ -545,112 +518,43 @@ public class Totoria extends AbstractWebSite {
 			logger.info("검색된 게시물('{}')의 첨부파일을 다운로드합니다.({})", boardItem.getTitle(), downloadLink);
 
 			try {
+				// 다운로드 받는 파일의 이름을 구한다.
+				String downloadFilePath = String.format("%s%s", this.downloadFileWriteLocation, downloadLink.getFileName().trim());
+				File downloadFile = new File(downloadFilePath);
+				if (downloadFile.exists() == true) {
+					logger.error("동일한 이름을 가진 파일이 이미 존재합니다. 해당 파일의 다운로드는 중지됩니다.({})", downloadFilePath);
+					continue;
+				}
+
+				File notyetDownloadFile = new File(downloadFilePath + Constants.AD_SERVICE_TASK_NOTYET_DOWNLOADED_FILE_EXTENSION);
+
 				/*
-				  다운로드 페이지로 이동
+				  첨부파일 다운로드
 				 */
-	            // @@@@@
-//				Connection.Response downloadProcess1Response = Jsoup.connect(DOWNLOAD_PROCESS_URL_1)
-//						.userAgent(USER_AGENT)
-//						.header("Referer", detailPageURL)
-//		                .data("down", downloadLink.getValue1())
-//		                .data("filetype", downloadLink.getValue4())
-//		                .data("file_id", downloadLink.getFileId())
-//		                .data("article_id", downloadLink.getId())
-//		                .method(Connection.Method.POST)
-//		                .cookies(this.loginConnResponse.cookies())
-//		                .timeout(URL_CONNECTION_TIMEOUT_LONG_MILLISECOND)
-//		                .ignoreContentType(true)
-//		                .execute();
-//
-//				if (downloadProcess1Response.statusCode() != HttpStatus.SC_OK)
-//					throw new IOException("POST " + DOWNLOAD_PROCESS_URL_1 + " returned " + downloadProcess1Response.statusCode() + ": " + downloadProcess1Response.statusMessage());
-//
-//				String result = downloadProcess1Response.parse().body().html();
-//				DownloadProcess1Result downloadProcess1Result = gson.fromJson(result, DownloadProcess1Result.class);
-//				if (StringUtil.isBlank(downloadProcess1Result.getStat()) == true || StringUtil.isBlank(downloadProcess1Result.getKey()) == true ||
-//						downloadProcess1Result.getStat().equals("true") == false) {
-//					throw new JsonParseException(String.format("첨부파일을 다운로드 하기 위한 작업 진행중에 수신된 데이터의 값이 유효하지 않습니다.(%s)", result));
-//				}
-//
-//				/*
-//				  다운로드 링크 페이지 열기
-//				 */
-//				String downloadProcessURL2 = String.format("%s%s", DOWNLOAD_PROCESS_URL_2, downloadProcess1Result.getKey());
-//				Connection.Response downloadProcess2Response = Jsoup.connect(downloadProcessURL2)
-//						.userAgent(USER_AGENT)
-//						.data("dddd", downloadLink.getValue1())
-//		                .data("vvvv", downloadLink.getValue2())
-//		                .data("ssss", downloadLink.getValue3())
-//		                .data("code", downloadProcess1Result.getKey())
-//		                .data("file_id", downloadLink.getFileId())
-//		                .data("valid_id", downloadProcess1Result.getMsg())
-//		                .method(Connection.Method.POST)
-//		                .cookies(this.loginConnResponse.cookies())
-//						.timeout(URL_CONNECTION_TIMEOUT_LONG_MILLISECOND)
-//		                .execute();
-//
-//				if (downloadProcess2Response.statusCode() != HttpStatus.SC_OK)
-//					throw new IOException("POST " + downloadProcessURL2 + " returned " + downloadProcess2Response.statusCode() + ": " + downloadProcess2Response.statusMessage());
-//
-//				Document downloadProcess2Doc = downloadProcess2Response.parse();
-//
-//				// 다운로드 받는 파일의 이름을 구한다.
-//				Elements elements = downloadProcess2Doc.select("#fileDetail p");
-//				if (elements.size() != 3)
-//					throw new ParseException(String.format("<P> 태그의 갯수 불일치로 파일명 추출이 실패하였습니다.(추출된갯수:%d) CSS셀렉터를 확인하세요.", elements.size()), 0);
-//
-//				String fileName = elements.get(1).text().trim();
-//				if (fileName.startsWith("Filename:") == false)
-//					throw new ParseException(String.format("추출된 문자열이 특정 문자열로 시작되지 않아 파일명 추출이 실패하였습니다.(추출된문자열:%s) CSS셀렉터를 확인하세요.", fileName), 0);
-//
-//				String downloadFilePath = String.format("%s%s", this.downloadFileWriteLocation, fileName.replace("Filename:", "").trim());
-//				File downloadFile = new File(downloadFilePath);
-//				if (downloadFile.exists() == true) {
-//					logger.error("동일한 이름을 가진 파일이 이미 존재합니다. 해당 파일의 다운로드는 중지됩니다.({})", downloadFilePath);
-//					continue;
-//				}
-//
-//				File notyetDownloadFile = new File(downloadFilePath + Constants.AD_SERVICE_TASK_NOTYET_DOWNLOADED_FILE_EXTENSION);
-//
-//				// 실제 다운로드 사이트에서 10초 대기후에 다운로드 받을 수 있으므로, 프로그램에서도 10초를 대기한다.
-//				// 이렇게 하지 않으면 첨부파일 다운로드가 실패하는 경우가 종종 발생한다.
-//				Thread.sleep(10000);
-//
-//				/*
-//				  첨부파일 다운로드 하기
-//				 */
-//				Connection.Response downloadProcess3Response = Jsoup.connect(DOWNLOAD_PROCESS_URL_3)
-//						.userAgent(USER_AGENT)
-//						.header("Referer", downloadProcessURL2)
-//		                .data("dddd", downloadProcess2Doc.select("input[id=dddd]").val())
-//		                .data("vvvv", downloadProcess2Doc.select("input[id=vvvv]").val())
-//		                .data("file_id", downloadProcess2Doc.select("input[id=file_id]").val())
-//		                .data("valid_id", downloadProcess2Doc.select("input[id=valid_id]").val())
-//		                .method(Connection.Method.POST)
-//		                .cookies(downloadProcess2Response.cookies())
-//		                .timeout(URL_CONNECTION_TIMEOUT_SHORT_MILLISECOND)
-//		                .ignoreContentType(true)
-//		                .execute();
-//
-//				if (downloadProcess3Response.statusCode() != HttpStatus.SC_OK)
-//					throw new IOException("POST " + DOWNLOAD_PROCESS_URL_3 + " returned " + downloadProcess3Response.statusCode() + ": " + downloadProcess3Response.statusMessage());
-//
-//				if (downloadProcess3Response.parse().text().contains("Unauthorized Access") == true)
-//					throw new ParseException("첨부파일 다운로드 결과로 Unauthorized Access가 반환되었습니다.", 0);
-//
-//				/*
-//				  첨부파일 저장
-//				 */
-//				FileOutputStream fos = new FileOutputStream(notyetDownloadFile);
-//				fos.write(downloadProcess3Response.bodyAsBytes());
-//				fos.close();
-//
-//                notyetDownloadFile.renameTo(downloadFile);
-//
-//				++downloadCompletedCount;
-//				downloadLink.setDownloadCompleted(true);
-//
-//				logger.info("검색된 게시물('{}')의 첨부파일 다운로드가 완료되었습니다.({})", boardItem.getTitle(), downloadFilePath);
+				Connection.Response downloadProcess3Response = Jsoup.connect(downloadLink.getLink())
+						.userAgent(USER_AGENT)
+		                .method(Connection.Method.POST)
+						.cookies(this.loginConnResponse.cookies())
+		                .timeout(URL_CONNECTION_TIMEOUT_LONG_MILLISECOND)
+		                .ignoreContentType(true)
+		                .execute();
+
+				if (downloadProcess3Response.statusCode() != HttpStatus.SC_OK)
+					throw new IOException("POST " + downloadLink.getLink() + " returned " + downloadProcess3Response.statusCode() + ": " + downloadProcess3Response.statusMessage());
+
+				/*
+				  첨부파일 저장
+				 */
+				FileOutputStream fos = new FileOutputStream(notyetDownloadFile);
+				fos.write(downloadProcess3Response.bodyAsBytes());
+				fos.close();
+
+                notyetDownloadFile.renameTo(downloadFile);
+
+				++downloadCompletedCount;
+				downloadLink.setDownloadCompleted(true);
+
+				logger.info("검색된 게시물('{}')의 첨부파일 다운로드가 완료되었습니다.({})", boardItem.getTitle(), downloadFilePath);
 			} catch (final Exception e) {
 				logger.error(String.format("첨부파일 다운로드 중에 예외가 발생하였습니다.(%s, %s)", boardItem, downloadLink), e);
 			}
