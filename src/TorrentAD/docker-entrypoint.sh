340b8c5502eca089bca64cc42f0e28c6dbb2ce3e
DarkKaiser <darkkaiser@gmail.com>
2023-06-20 12:09:25 +0900

    도커가 실행될 때 설정파일의 권한이 유지되도록 변경

--- src/TorrentAD/docker-entrypoint.sh
+++ src/TorrentAD/docker-entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 
-APP_JAR_FILE=/docker-entrypoint/dist/torrentad-1.1.0.jar
+APP_PATH=/usr/local/app/
+APP_JAR_FILE=/usr/local/app/torrentad-1.1.0.jar
 
-if [ -f "$APP_JAR_FILE" ]; then
-  mv -f $APP_JAR_FILE /usr/local/app/
+LATEST_APP_JAR_FILE=/docker-entrypoint/dist/torrentad-1.1.0.jar
+
+if [ -f "$LATEST_APP_JAR_FILE" ]; then
+  mv -f $LATEST_APP_JAR_FILE $APP_PATH
+  chown +1000:staff $APP_JAR_FILE
 fi
 
 exec "$@"
